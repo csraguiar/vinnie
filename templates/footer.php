b906e20f0f58eaa644bcf12aca75b7d30ff2f320
Cesar <hi@cesaraguiar.me>
2015-03-06 11:18:09 +0000

    Added link to GitHub repo.

--- templates/footer.php
+++ templates/footer.php
@@ -2,7 +2,7 @@
 <div class="wrap">
 <?php dynamic_sidebar('sidebar-footer'); ?>
 
-<p>This theme was made by me. You're free to hack it.</p>
+<p>This theme was made by me. You're free to <a href="https://github.com/csraguiar/vinnie">hack it</a>.</p>
 <p><a href="http://twitter.com/csraguiar">@csraguiar</a></p>
 
 </div>
